2839df4a5e836af9c0f226018e2b293efd028284
Justas <zdanaviciusjustas1@gmail.com>
2021-06-06 11:27:13 +0200

    packages

--- scripts/jukebox.js
+++ scripts/jukebox.js
@@ -3,6 +3,9 @@ function render(){
                     ["https://images-na.ssl-images-amazon.com/images/I/A1LVEJikmZL._SL1500_.jpg", "Currents", "Tame Impala"], 
                     ["https://i2.wp.com/thesewaneepurple.org/wp-content/uploads/2020/02/The-Slow-Rush-album-cover.-Photo-from-Consequence-of-Sound..jpg?fit=3000%2C3000&ssl=1", "The Slow Rush", "Tame Impala"]];
 
+    var songElements = [];
+    var selected;
+
     var jukebox = document.getElementById("jukebox-content");
 
     for(let i = 0; i < songList.length; i++){
@@ -25,14 +28,19 @@ function render(){
         title.classList.add("jukebox-song-title");
         artist.classList.add("jukebox-song-artist");
 
+        song.style.opacity = 1;
+
+        songElements.push(song);
         jukebox.appendChild(song);
 
         song.onclick = async function () {
-            var selected = i;
+            selected = i;
 
-            fadeTo(songList[i], 1);
-            fadeTo(songList[i-1], 0.5);
-            fadeTo(songList[i+1], 0.5);
+            fadeTo(songElements[i-2], 0);
+            fadeTo(songElements[i-1], 0.5);
+            fadeTo(songElements[i], 1);
+            fadeTo(songElements[i+1], 0.5);
+            fadeTo(songElements[i+2], 0);
 
             while (song.getBoundingClientRect().left + (song.getBoundingClientRect().right - song.getBoundingClientRect().left)/2 < window.innerWidth/2){
                 let newLeft = (+jukebox.style.left.substring(0, jukebox.style.left.length-2) + 10) + "px";
@@ -47,23 +55,26 @@ function render(){
             }
         }
     }
+
+    console.log(songElements);
 }
+
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function fadeTo(song, opacity){
+    console.log(song);
     if (song == undefined || song.style == undefined) return;
 
-    let currentOpacity = song.style.opacity;
-    while (opacity > currentOpacity){
+    while (opacity > song.style.opacity){
         //Fade out
-        song.style.opacity = 0;
-    }
-    while (opacity < currentOpacity){
-        //Fade in
         song.style.opacity += 0.1;
-
     }
+    while (opacity < song.style.opacity){
+        //Fade in
+        song.style.opacity -= 0.1;
+    }
+    console.log("SUCCESS!");
 }
