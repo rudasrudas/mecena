473f0cfc51868c47220c0710d5d56462f0041d04
Justas <zdanaviciusjustas1@gmail.com>
2021-08-08 17:41:12 +0200

    Post request example setup

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,25 +1,23 @@
 window.onload = function() {
     const form = document.getElementById('contact-form');
-    const name = document.getElementById('contact-name');
-    const email = document.getElementById('contact-email');
-    const message = document.getElementById('contact-message');
-
-    form.onsubmit = sendMessage(name, email, message);
-    console.log('ln8');
+    // form.onsubmit = sendMessage();
 }
 
-function sendMessage(name, email, message) {
+function sendMessage() {
+    let name = document.getElementById('contact-name').value;
+    let email = document.getElementById('contact-email').value;
+    let message = document.getElementById('contact-message').value;
 
     const xhr = new XMLHttpRequest();
-    xhr.open('POST', `http://45.80.152.150:3000/send-message`, true);
+    xhr.open('POST', `http://45.80.152.150:5000/send-message`, true);
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.onload = function() {
         if (xhr.status === 200) {
+            console.log(xhr.response);
             alert(xhr.response);
-            console.log(this.responseText);
         }
         else {
-            alert('Request failed.  Returned status of ' + xhr.status);
+            alert('Request failed. Returned status of ' + xhr.status);
         }
     };
     xhr.send(
