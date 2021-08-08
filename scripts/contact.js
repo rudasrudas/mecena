8790295d559994c583f28b183de6f699dfe602db
Justas <zdanaviciusjustas1@gmail.com>
2021-08-08 16:44:47 +0200

    Loading images from the server

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,14 +1,32 @@
-function sendMessage() {
+window.onload = function() {
+    const form = document.getElementById('contact-form');
+    const name = document.getElementById('contact-name');
+    const email = document.getElementById('contact-email');
+    const message = document.getElementById('contact-message');
+
+    form.onsubmit = sendMessage(name, email, message);
+    console.log('ln8');
+}
+
+function sendMessage(name, email, message) {
 
     const xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://45.80.152.150:5000/', true);
+    xhr.open('POST', `http://45.80.152.150:3000/send-message`, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
     xhr.onload = function() {
         if (xhr.status === 200) {
             alert(xhr.response);
+            console.log(this.responseText);
         }
         else {
             alert('Request failed.  Returned status of ' + xhr.status);
         }
     };
-    xhr.send();
+    xhr.send(
+        JSON.stringify({
+            name: name,
+            email: email,
+            message: message
+        }
+    ));
 }
--- scripts/jukebox.js
+++ scripts/jukebox.js
@@ -1,62 +1,83 @@
 function render(){
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', `http://45.80.152.150:5000/featured`, true);
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            const tracks = JSON.parse(xhr.response);
+
+            var songElements = [];
+            var selected;
+
+            var jukebox = document.getElementById("jukebox-content");
+
+            for(let i = 0; i < songList.length; i++){
+                const song = document.createElement("div");
+
+                const cover = document.createElement("img");
+                const title = document.createElement("h4");
+                const artist = document.createElement("h4");
+
+                song.appendChild(cover);
+                song.appendChild(title);
+                song.appendChild(artist);
+
+                // cover.src = songList[i][0];
+                // title.innerHTML = songList[i][1];
+                // artist.innerHTML = songList[i][2];
+                
+                // getArtwork(cover.src, tracks[i].cover);
+                cover.src = tracks[i].cover;
+                title.innerHTML = tracks[i].title;
+                artist.innerHTML = tracks[i].artist;
+
+                song.classList.add("jukebox-song");
+                cover.classList.add("jukebox-song-img");
+                title.classList.add("jukebox-song-title");
+                artist.classList.add("jukebox-song-artist");
+
+                song.style.opacity = 1;
+
+                songElements.push(song);
+                jukebox.appendChild(song);
+
+                song.onclick = async function () {
+                    selected = i;
+
+                    fadeTo(songElements[i-2], 0);
+                    fadeTo(songElements[i-1], 0.5);
+                    fadeTo(songElements[i], 1);
+                    fadeTo(songElements[i+1], 0.5);
+                    fadeTo(songElements[i+2], 0);
+
+                    while (song.getBoundingClientRect().left + (song.getBoundingClientRect().right - song.getBoundingClientRect().left)/2 < window.innerWidth/2){
+                        let newLeft = (+jukebox.style.left.substring(0, jukebox.style.left.length-2) + 10) + "px";
+                        jukebox.style.left = newLeft;
+
+                        await sleep(1);
+                    }
+                    while (song.getBoundingClientRect().left + (song.getBoundingClientRect().right - song.getBoundingClientRect().left)/2 > window.innerWidth/2){
+                        let newLeft = (+jukebox.style.left.substring(0, jukebox.style.left.length-2) - 10) + "px";
+                        jukebox.style.left = newLeft;
+                        await sleep(1);
+                    }
+                }
+            }
+
+            console.log(songElements);
+        }
+        else {
+            alert('Request failed.  Returned status of ' + xhr.status);
+        }
+    };
+    xhr.send();
+
     var songList = [["https://preview.redd.it/wzfk3rqz36d41.png?auto=webp&s=0ef0940179edeb0548fcfc3c062ed81bdc63e185", "Trilogy kiss", "The Weeknd"],
                     ["https://images-na.ssl-images-amazon.com/images/I/A1LVEJikmZL._SL1500_.jpg", "Currents", "Tame Impala"], 
                     ["https://i2.wp.com/thesewaneepurple.org/wp-content/uploads/2020/02/The-Slow-Rush-album-cover.-Photo-from-Consequence-of-Sound..jpg?fit=3000%2C3000&ssl=1", "The Slow Rush", "Tame Impala"]];
 
-    var songElements = [];
-    var selected;
+    
 
-    var jukebox = document.getElementById("jukebox-content");
-
-    for(let i = 0; i < songList.length; i++){
-        let song = document.createElement("div");
-
-        let cover = document.createElement("img");
-        let title = document.createElement("h4");
-        let artist = document.createElement("h4");
-
-        song.appendChild(cover);
-        song.appendChild(title);
-        song.appendChild(artist);
-
-        cover.src = songList[i][0];
-        title.innerHTML = songList[i][1];
-        artist.innerHTML = songList[i][2];
-
-        song.classList.add("jukebox-song");
-        cover.classList.add("jukebox-song-img");
-        title.classList.add("jukebox-song-title");
-        artist.classList.add("jukebox-song-artist");
-
-        song.style.opacity = 1;
-
-        songElements.push(song);
-        jukebox.appendChild(song);
-
-        song.onclick = async function () {
-            selected = i;
-
-            fadeTo(songElements[i-2], 0);
-            fadeTo(songElements[i-1], 0.5);
-            fadeTo(songElements[i], 1);
-            fadeTo(songElements[i+1], 0.5);
-            fadeTo(songElements[i+2], 0);
-
-            while (song.getBoundingClientRect().left + (song.getBoundingClientRect().right - song.getBoundingClientRect().left)/2 < window.innerWidth/2){
-                let newLeft = (+jukebox.style.left.substring(0, jukebox.style.left.length-2) + 10) + "px";
-                jukebox.style.left = newLeft;
-
-                await sleep(1);
-            }
-            while (song.getBoundingClientRect().left + (song.getBoundingClientRect().right - song.getBoundingClientRect().left)/2 > window.innerWidth/2){
-                let newLeft = (+jukebox.style.left.substring(0, jukebox.style.left.length-2) - 10) + "px";
-                jukebox.style.left = newLeft;
-                await sleep(1);
-            }
-        }
-    }
-
-    console.log(songElements);
+    
 }
 
 
