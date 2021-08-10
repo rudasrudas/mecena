76d35d65c0c01d5c669c57e9ae8c7f38db83f960
youngdaggo <martynasgudz1@gmail.com>
2021-08-10 17:07:04 +0300

    2nd remake of the jukebox carousel

---
+++ scripts/owl.js
@@ -0,0 +1,19 @@
+var owl = $(".owl-carousel");
+var prevBtn = $("#jukebox-prev");
+var nextBtn = $("#jukebox-next");
+
+owl.owlCarousel({
+    center: true,
+    items: 3,
+    loop: true,
+    margin: 10,
+    dots: false,
+});
+
+prevBtn.click(() => {
+    owl.trigger("prev.owl.carousel")
+})
+
+nextBtn.click(() => {
+    owl.trigger("next.owl.carousel")
+})
