063d448f7c8c840ab38e738fa53fef465c1b209b
Justas <zdanaviciusjustas1@gmail.com>
2021-08-14 23:08:53 +0200

    Song setup in portfolio page

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -1,11 +1,12 @@
 //Wait for the page to load
 window.onload = function() {
-    
+    jumpToTheCenter();
 }
 
 //Immediately-invoked Function Expression
 ;(function(){
     getArtworks();
+    getTracks();
 })();
 
 function getArtworks() {
@@ -20,7 +21,7 @@ function getArtworks() {
             }
         }
         else {
-            alert('Request failed.  Returned status of ' + xhr.status);
+            alert('Request failed. Returned status of ' + xhr.status);
         }
     };
     xhr.send();
@@ -35,4 +36,39 @@ function appendArtwork(artwork, container){
     </div>`.trim();
 
     container.appendChild(div.firstChild);
+}
+
+function getTracks() {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', `https://api.mecena.net/tracks`, true);
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            const tracks = JSON.parse(xhr.response);
+            var container = $('#tracks')[0];
+            for(let i = 0; i < Object.keys(tracks).length; i++){
+                appendTrack(tracks[i], container);
+            }
+        }
+        else {
+            alert('Request failed. Returned status of ' + xhr.status);
+        }
+    };
+    xhr.send();
+}
+
+function appendTrack(track, container){
+    let div = document.createElement('div');
+    div.innerHTML = `
+    <div class="track">
+        <img class="track-img" src="https://api.mecena.net/artwork/${track.cover}">
+        <h4 class="track-label">${track.title}</h4>
+    </div>`.trim();
+
+    container.appendChild(div.firstChild);
+}
+
+function jumpToTheCenter(){
+    document.getElementById('splitter').scrollIntoView({
+        inline: "center"
+    });
 }
